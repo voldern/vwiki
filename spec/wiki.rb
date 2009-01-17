9e2376fc475d36ce3974881261efb172eadaaa3d
Espen Volden <voldern@harry.(none)>
2009-01-17 13:31:38 +0100

    Refactored the duplication of removing a field from the fixture data

--- spec/wiki.rb
+++ spec/wiki.rb
@@ -8,13 +8,22 @@ module Fixtures
 	Data = { 'date' => '2008-12-10', 'name' => 'Test', 'author' => 'voldern',
 		'body' => 'Lorem ipsum' }
 
-	def self.generate_page(data)
+	def self.generate_page(data = nil)
+		data = Data if data.nil?
+
 		page = Wiki::Page.new('test')
 		data.each do |name, data|
 			eval("page.#{name} = '#{data}'")
 		end
 
 		page
+	end
+
+	def self.generate_page_without(name, data = nil)
+		data = Data if data.nil?
+		data.delete(name.to_s)
+
+		generate_page(data)
 	end
 end
 
@@ -26,39 +35,31 @@ describe Wiki, ' a new wiki page' do
 	end
 
 	it "should be possible to save a valid page" do
-		page = Fixtures::generate_page(Fixtures::Data)
+		page = Fixtures::generate_page
 
 		page.save.should eql(Fixtures::Data)
 	end
 
 	it "should not be able to save with empty date" do
-		data = Fixtures::Data
-		data.delete('date')
-		page = Fixtures::generate_page(data)
+		page = Fixtures::generate_page_without :date
 
 		page.save.should eql(false)
 	end
 
 	it "should not be able to save with empty name" do
-		data = Fixtures::Data
-		data.delete('name')
-		page = Fixtures::generate_page(data)
+		page = Fixtures::generate_page_without :name
 
 		page.save.should eql(false)
 	end
 	
 	it "should not be able to save with empty author" do
-		data = Fixtures::Data
-		data.delete('author')
-		page = Fixtures::generate_page(data)
+		page = Fixtures::generate_page_without :author
 
 		page.save.should eql(false)
 	end
 
 	it "should not be able to save with empty body" do
-		data = Fixtures::Data
-		data.delete('body')
-		page = Fixtures::generate_page(data)
+		page = Fixtures::generate_page_without :body
 
 		page.save.should eql(false)
 	end
