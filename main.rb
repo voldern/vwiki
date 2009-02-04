0f89e22c90a47cb999de638ae32d661b1a5e71e9
Espen Volden <espenv@hardware.no>
2009-02-04 19:31:09 +0100

    Added index and new method to test some of the backend functions

--- main.rb
+++ main.rb
@@ -4,9 +4,27 @@ require 'rdiscount' # Markdown
 require 'lib/wiki'
 
 class MainController < Ramaze::Controller
+	engine :Haml
+
 	def index
-		"Hello World!"
+		@page = Wiki::Page.new('index')
 	end
+
+	def new
+		page = Wiki::Page.new('index')
+		page.date = Time.now
+		page.name = 'Index'
+		page.author = 'voldern'
+		page.body = 'Velkommen'
+		page.save!
+	end
+
+	def edit
+	end
+
+	def view(page)
+	end
+
 end
 
 Ramaze.start
